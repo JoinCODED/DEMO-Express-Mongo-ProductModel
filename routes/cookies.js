264142f72355c9fe02330c795432d5c35acf23cb
Lailz <lailaabdulraheem@gmail.com>
2020-05-05 00:07:56 +0300

    cleanup

--- routes/cookies.js
+++ routes/cookies.js
@@ -9,19 +9,14 @@ const {
   cookieDelete
 } = require("../controllers/cookieController");
 
-// Cookie Create
 router.post("/", cookieCreate);
 
-// Cookie List
 router.get("/", cookieList);
 
-// Cookie Detail
 router.get("/:cookieId", cookieDetail);
 
-// Cookie Update
 router.put("/:cookieId", cookieUpdate);
 
-// Cookie Delete
 router.delete("/:cookieId", cookieDelete);
 
 module.exports = router;
